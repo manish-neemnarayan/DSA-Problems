213e031b29cbc94cbdc71369fa36047394a90a83
manish-neemnarayan <manish9301kumar@gmail.com>
2023-07-24 21:41:32 +0530

    max count

--- Arrays/maxCountOnes.js
+++ Arrays/maxCountOnes.js
@@ -1,21 +1,22 @@
-let arr = [1,0,1,1,0,0,0,1,1,1,1,0];
-
-function maxCountOnes(arr) {
-    let counter = 0;
-    let final = 0;
-    
+let arr = [4,3,2,7,7,2,3,1];
+// arr values range in [1,n.length];
+function missingValues(arr) {
+  let output = [];
+  
   for(let i=0; i<arr.length; i++) {
-
-    if(arr[i]==1){
-      counter++;
-    } else{
-      if(final<counter) {
-        final = counter;
-      }
-      counter = 0;
+    let arrVal = Math.abs(arr[i]);
+    if(arr[arrVal-1] > 0){
+    	arr[arrVal-1] = -(arr[arrVal-1]);  
     }
   }
-  console.log(final);
+  console.log(arr)
+  
+	for(let j=0; j<arr.length; j++){
+    if(arr[j] !== -Math.abs(arr[j])){
+      output.push(j+1);
+    }
+  }
+  console.log(output);
 }
 
-maxCountOnes(arr);
+missingValues(arr);
