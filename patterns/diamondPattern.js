713702822403013398cd91cbbfd902868128291f
manish-neemnarayan <manish9301kumar@gmail.com>
2023-07-24 11:30:58 +0530

    pattern added

---
+++ patterns/diamondPattern.js
@@ -0,0 +1,37 @@
+let count = 7;
+
+function diamondPatternAbove(n) {
+  for(let i=1; i<n; i++){
+    let spaces = i-1;
+    let pattern = ' ';
+
+    for(let s=0; s<=spaces; s++){
+        pattern += ` `;
+    }
+    for(let j=i; j<=n; j++){
+      pattern += `${j} `;
+    }
+    console.log(pattern);
+  }
+}
+
+function diamondPatternBelow(n) {
+    for(let i=n; i>=1; i--){
+      let spaces = i-1;
+      let pattern = ' ';
+  
+      for(let s=0; s<=spaces; s++){
+          pattern += ` `;
+       }
+      for(let j=i; j<=n; j++){
+        pattern += `${j} `;
+      }
+      console.log(pattern);
+    }
+}
+
+function diamondPattern(count) {
+    diamondPatternAbove(count);
+    diamondPatternBelow(count);
+}
+diamondPattern(count);
