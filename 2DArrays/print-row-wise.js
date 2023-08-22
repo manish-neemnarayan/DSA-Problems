40e7f8d5c9b08f94b4d1611d69da4dff22b805df
manish-neemnarayan <manish9301kumar@gmail.com>
2023-08-22 18:21:26 +0530

    print-row-wise-2DArr

---
+++ 2DArrays/print-row-wise.js
@@ -0,0 +1,17 @@
+let arr = [
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9, 10, 11, 12],
+];
+
+function printRowWise(arr) {
+  console.log(arr);
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      console.log(arr[i][j]);
+    }
+  }
+}
+
+printRowWise(arr);
